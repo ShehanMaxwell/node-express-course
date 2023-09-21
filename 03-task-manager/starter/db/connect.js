f165aeb0e77a052f4d93aff1c33fa7828cf443e3
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-21 17:28:36 +0530

    modified app.js and connect.js

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-  'mongodb+srv://shehanmaxwell:jZZiv5zFm2zZ6rhO@nodeandexpressprojects.4popwk8.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true, //to remove log error warnings
     useCreateIndex: true, //to remove log error warnings
     useFindAndModify: false, //to remove log error warnings
