e3764a0c883ab9e0553f1e43e91e7c93e8cfab98
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-26 18:25:10 +0530

    modified app.js added not-found.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -15,11 +15,8 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
 
-//app.get('/api/v1/tasks')   - get all the tasks
-//app.post('/api/v1/tasks')  - create a new task
-//app.get('/api/v1/tasks/:id') - get single task
-//app.patch('/api/v1/tasks/:id') - update task
-//app.delete('/api/v1/tasks/:id') - delete task
+app.use()
+
 
 const port = 3000;
 
