61c9b60cf0426f46663e0643e0a48835dafe8189
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-26 19:58:39 +0530

    modified app.js and not-found.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 // middleware
 app.use(express.static('./public'));
@@ -15,8 +16,7 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
 
-app.use()
-
+app.use(notFound);
 
 const port = 3000;
 
