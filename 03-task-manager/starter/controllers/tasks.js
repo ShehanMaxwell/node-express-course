2c37269aba8618f9d18bb2cc71d355bcda239c86
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-26 12:24:32 +0530

    modified tasks.js in controllers

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -46,8 +46,19 @@ const updateTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
 
-    res.status(200).json({ id: taskID, data: req.body });
-  } catch (error) {}
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
