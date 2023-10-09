021bc8a331790ad33930787de79dc3bb9c2858aa
Shehan Maxwell <shehanbbn@gmail.com>
2023-10-09 13:20:08 +0530

    modified products.js in controllers

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,12 +2,22 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
-    name: 'vase table',
+    page: '2',
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if(company){
+    queryObject.company = company
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
