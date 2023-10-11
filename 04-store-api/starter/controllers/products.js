5d4752a582a4222dfba94d78c417d0f3a9bf856f
Shehan Maxwell <shehanbbn@gmail.com>
2023-10-11 20:09:59 +0530

    modified products.js in controllers

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,11 +1,10 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort('name')
-    .select('name price')
-    .limit(10)
-    .skip(5);
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('price')
+    .select('name price');
+
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
@@ -35,14 +34,11 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
-  const page = Number(req, query.page) || 1;
+  const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
-
-
-  
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
