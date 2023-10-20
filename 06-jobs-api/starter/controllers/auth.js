4568b1ae1fcfd79361f60847d773e2056d593ba1
Shehan Maxwell <shehanbbn@gmail.com>
2023-10-20 12:54:50 +0530

    modified auth.js and User.js

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,13 +1,30 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email & password');
+  }
+
+  const user = await User.findOne({ email });
+  //compare password
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({user:{name:user.name},token})
+
+
 };
 
 module.exports = {
